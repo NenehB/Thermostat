fc7d22df886c3b55ca0bff713580f33330f29ba3
NenehB <Neneh.Bojang@digital.justice.gov.uk>
2021-11-16 12:52:13 +0000

    Fizzbuzz completed

--- FizzBuzz/spec/fizzBuzzSpec.js
+++ FizzBuzz/spec/fizzBuzzSpec.js
@@ -1,7 +1,41 @@
 describe("Fizzbuzz", function() {
-  let fizzbuzz;
-  it("should be divisible by 3 ", function() {
+  
+  beforeEach(function(){
     fizzbuzz = new Fizzbuzz();
-    expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
   });
+
+  describe("Fizzbuzz", function() {
+    it("should be divisible by 3 ", function() {
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.fizz_number(3)).toEqual("Fizz");
+    });
+  });
+
+  describe("Fizzbuzz", function() {
+    it("should NOT be divisible by 3 ", function() {
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.fizz_number(4)).toEqual(4);
+    });
+  });
+  
+  describe("Fizzbuzz", function() {
+    it("should be divisible by 5 ", function() {
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.fizz_number(10)).toEqual("Buzz");
+    });
+  });
+  
+  describe("Fizzbuzz", function() {
+    it("should NOT be divisible by 5 ", function() {
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.fizz_number(13)).toEqual(13);
+    });
+  });
+  describe("Fizzbuzz", function() {
+    it("should be divisible by 15 ", function() {
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.fizz_number(15)).toEqual("Fizzbuzz");
+    });
+  });
+  
 });
--- FizzBuzz/src/fizzBuzz.js
+++ FizzBuzz/src/fizzBuzz.js
@@ -1,14 +1,28 @@
 class Fizzbuzz {
-  
-  
-  isDivisibleByThree(number){
-    if (number % 3 == 0 ){
-      return true;
+
+  fizz_number(number){
+    if (this._isDivisibleBy(number, 15)){
+      return "Fizzbuzz";
+    }
+    else if (this._isDivisibleBy(number, 5)) {
+      return "Buzz";
+    }
+    else if (this._isDivisibleBy(number, 3)) {
+      return "Fizz";
     }
     else {
-      return false;
+      return number;
     }
-
+  
   }
 
+  _isDivisibleBy(number, divisor){
+    return (number % divisor === 0);
+  }
+
+}
+let fizzBuzz = new Fizzbuzz();
+
+for (let i = 1; i <= 100; i++) {
+  console.log(fizzBuzz.fizz_number(i));
 }
